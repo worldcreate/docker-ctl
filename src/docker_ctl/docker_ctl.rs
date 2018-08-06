39bb2a20df408fc4b02e6fd4bcc89f90a22dd137
matsumoto <r-matsumoto@zyyx.jp>
2018-08-07 00:58:51 +0900

    option parserの処理の移動

--- src/docker_ctl/docker_ctl.rs
+++ src/docker_ctl/docker_ctl.rs
@@ -1,10 +1,41 @@
+extern crate clap;
+use self::clap::{Arg, App, SubCommand};
 
 pub struct DockerCtl {
-
+    
 }
 
 impl DockerCtl {
     pub fn new() -> Result<DockerCtl, &'static str> {
+        let matches = App::new("docker ctl")
+                              .version("0.1")
+                              .about("docker ctl")
+                              .arg(Arg::with_name("v")
+                                   .short("v")
+                                   .help("Sets the level of verbosity"))
+                              .subcommand(SubCommand::with_name("test")
+                                          .arg(Arg::with_name("INPUT")
+                                               .help("Sets the input file to use")
+                                               .required(true)
+                                               .index(1)))
+                              .get_matches();
+
+
+        // Vary the output based on how many times the user used the "verbose" flag
+        // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+        let is_verbose = matches.is_present("v");
+
+        // You can handle information about subcommands by requesting their matches by name
+        // (as below), requesting just the name used, or both at the same time
+        if let Some(matches) = matches.subcommand_matches("test") {
+            // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+            // required we could have used an 'if let' to conditionally get the value)
+            println!("arg INPUT = {}", matches.value_of("INPUT").unwrap());
+        }
         Ok(DockerCtl {})
+    }
+
+    pub fn run(&self) {
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,14 @@
-extern crate clap;
-use clap::{Arg, App, SubCommand};
-
 use docker_ctl::DockerCtl;
 
 mod docker_ctl;
 
 fn main() {
-    DockerCtl::new();
-
-    let matches = App::new("docker ctl")
-                          .version("0.1")
-                          .about("docker ctl")
-                          .arg(Arg::with_name("v")
-                               .short("v")
-                               .help("Sets the level of verbosity"))
-                          .subcommand(SubCommand::with_name("test")
-                                      .arg(Arg::with_name("INPUT")
-                                           .help("Sets the input file to use")
-                                           .required(true)
-                                           .index(1)))
-                          .get_matches();
-
-
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    let is_verbose = matches.is_present("v");
-
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("test") {
-        // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-        // required we could have used an 'if let' to conditionally get the value)
-        println!("arg INPUT = {}", matches.value_of("INPUT").unwrap());
-    }
-
+    match DockerCtl::new() {
+        Ok(docker_ctl) => {
+           docker_ctl.run();
+        },
+        Err(error) => {
+            panic!(error);
+        }
+    };
 }
